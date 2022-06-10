b7c5be702f363563cc2a2db2f457b9547ce01854
Leandro Peixoto de Oliveira <loliveira@gasbrasiliano.com.br>
2022-06-10 15:43:13 -0300

    teste 1º trim

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import openpyxl
 import pandas as pd
 from datetime import datetime
-import calendar
 
 wb = openpyxl.load_workbook('DF2.xlsx')  # Abrir arquivo com a base
 sheets = wb.sheetnames
 ws = wb['balancetes']
 
-data_usuario = '31/01/2022'
+data_usuario = '31/03/2022'
 
 data_cur = datetime.strptime(data_usuario, '%d/%m/%Y')
 
@@ -59,7 +58,8 @@ razao = pd.DataFrame(razao)
 razao.fillna('', inplace=True)
 razao['Texto'] = razao['Texto'].str.lower()
 
-ppr, ppr1, ppr2, outras_rec, outras_desp, ir_dif, csll_dif, indebito = 0, 0, 0, 0, 0, 0, 0, 0
+ppr, ppr1, ppr2, outras_rec, outras_desp, ir_dif, csll_dif, indebito, prov_ind, deliberacao, termok, vag, vag2 = \
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 for index, row in razao.iterrows():
     if row['Atribuição'] == '615.03.1.1.01.01' and ('ppr' in row['Texto'] or 'partic' in row['Texto']):
@@ -87,6 +87,21 @@ for index, row in razao.iterrows():
     elif row['Atribuição'] == '710.00.2.2' and 'dif' in row['Texto']:
         lista6 = razao.loc[index, 'Montante em moeda interna']
         csll_dif += lista6
+    elif row['Atribuição'] == '675.03.0.3' and 'pis' in row['Texto']:
+        lista_pis = razao.loc[index, 'Montante em moeda interna']
+        prov_ind += lista_pis
+    elif row['Atribuição'] == '675.03.0.3' and 'conta' in row['Texto']:
+        lista_conta = razao.loc[index, 'Montante em moeda interna']
+        deliberacao += lista_conta
+    elif row['Atribuição'] == '675.03.0.3' and 'ajustek' in row['Texto']:
+        lista_ajustek = razao.loc[index, 'Montante em moeda interna']
+        termok += lista_ajustek
+    elif row['Atribuição'] == '652.00.0.1.01' and 'vag' in row['Texto']:
+        lista_vag = razao.loc[index, 'Montante em moeda interna']
+        vag += lista_vag
+    elif row['Atribuição'] == '651.00.0.1.01' and 'vag' in row['Texto']:
+        lista_vag2 = razao.loc[index, 'Montante em moeda interna']
+        vag2 += lista_vag2
 
 
 # for row in ws.iter_rows(min_row=6, min_col=6, max_col=6, max_row=727):
@@ -104,77 +119,76 @@ for index, row in razao.iterrows():
 balancete = pd.read_excel(f'balancete{data_cur.month}.xlsx')
 balancete = pd.DataFrame(balancete)
 
-lista_50 = []
+lista_plano50 = []
 lista_pisco = []
 
 # lista de contas 'diferença de PIS/COFINS'
-busca = ['6110100004', '6112100004', '6113100004', '6114100004', '6116100004']
+contas_pisco = ['6110100004', '6112100004', '6113100004', '6114100004', '6116100004']
 
 # Listar contas 615 (C/C, compensação) e dividir por segmento
-busca2 = [['6152131002', '6152131004'], ['6150131002', '6150131004', '6153131002', '6153131004'],
-          ['6154131002', '6154131004'], ['6156131002', '6156131004']]
+contas_comp = [['6152131002', '6152131004'], ['6150131002', '6150131004', '6153131002', '6153131004'],
+               ['6154131002', '6154131004'], ['6156131002', '6156131004']]
 
 # dicionario para somar os valores nas contas chave
-dicionario = {'6152131002': 0, '6153131002': 0, '6154131002': 0, '6156131002': 0}
+contas_custo = {'6152131002': 0, '6153131002': 0, '6154131002': 0, '6156131002': 0}
 
 for index, row in balancete.iterrows():
-    if row['Conta do Razão'] in busca:
+    if row['Conta do Razão'] in contas_pisco:
         lista_pisco.append(balancete.loc[index, 'Saldo Acumulado'])
-    elif row['Conta do Razão'] in busca2[0]:
-        dicionario['6152131002'] += balancete.loc[index, 'Saldo Acumulado']
-    elif row['Conta do Razão'] in busca2[1]:
-        dicionario['6153131002'] += balancete.loc[index, 'Saldo Acumulado']
-    elif row['Conta do Razão'] in busca2[2]:
-        dicionario['6154131002'] += balancete.loc[index, 'Saldo Acumulado']
-    elif row['Conta do Razão'] in busca2[3]:
-        dicionario['6156131002'] += balancete.loc[index, 'Saldo Acumulado']
+    elif row['Conta do Razão'] in contas_comp[0]:
+        contas_custo['6152131002'] += balancete.loc[index, 'Saldo Acumulado']
+    elif row['Conta do Razão'] in contas_comp[1]:
+        contas_custo['6153131002'] += balancete.loc[index, 'Saldo Acumulado']
+    elif row['Conta do Razão'] in contas_comp[2]:
+        contas_custo['6154131002'] += balancete.loc[index, 'Saldo Acumulado']
+    elif row['Conta do Razão'] in contas_comp[3]:
+        contas_custo['6156131002'] += balancete.loc[index, 'Saldo Acumulado']
     elif row['Conta do Razão'] == '1123120000':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
     elif row['Conta do Razão'] == '1123120001':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
     elif row['Conta do Razão'] == '6122102002':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
     elif row['Conta do Razão'] == '6123102002':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
     elif row['Conta do Razão'] == '6160191040':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
     elif row['Conta do Razão'] == '6160191041':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
     elif row['Conta do Razão'] == '6160351011':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
     elif row['Conta do Razão'] == '6310500002':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
     elif row['Conta do Razão'] == '6350600009':
-        lista_50.append(balancete.loc[index, 'Saldo Acumulado'])
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
+    elif row['Conta do Razão'] == '6520101001':
+        lista_plano50.append(balancete.loc[index, 'Saldo Acumulado'])
 
-
-print(lista_50)
+print(lista_plano50)
 # =====================================================================================================#
 
 dados = pd.read_excel(f'G:\GECOT\CONCILIAÇÕES CONTÁBEIS\CONCILIAÇÕES_2022\BALANCETES\SOCIETÁRIOS\\'
-                      f'Balancete 0{data_cur.month}2022.xlsx', skiprows=7)
+                      f'Balancete 0{data_cur.month}2022 rev.02.xlsx', skiprows=12)
 
 dados = pd.DataFrame(dados)
 
 dados['Conta CSPE'] = dados['Conta CSPE'].astype(str)
 
 conciliacoes = ['121.51.9', '121.83.2', '211.21.1', '211.21.4', '211.61.1', '211.99.9', '221.61.1', '221.99.3',
-                '213.03.1']
+                '223.03.1']
 razao = []
 caminho = f'G:\GECOT\CONCILIAÇÕES CONTÁBEIS\CONCILIAÇÕES_2022\\0{data_cur.month}.2022\\' + 'Conta '
 
 # cont, cont2, cont3, cont4, cont5, cont6, cont7, cont8 = 0, 0, 0, 0, 0, 0, 0, 0
-for row in ws.iter_rows(min_row=6, min_col=6, max_col=6, max_row=727):
+for row in ws.iter_rows(min_row=6, min_col=6, max_col=6, max_row=len(ws['F'])):
     for index, cell in enumerate(row):
         if ws.cell(row=cell.row, column=6).value == ws.cell(row=cell.row - 1, column=6).value:
             break
         elif cell.value not in conciliacoes and cell.value not in razao:
-            for index, row2 in dados.iterrows():
+            for index2, row2 in dados.iterrows():
                 if str(cell.value) == row2['Conta CSPE']:
-                    ws.cell(row=cell.row, column=pos_coluna).value = dados.loc[index, ' Saldo Acumulado']
+                    ws.cell(row=cell.row, column=pos_coluna).value = dados.loc[index2, ' Saldo Acumulado']
                     ws.cell(row=cell.row, column=pos_coluna).number_format = '#,##0.00'
-
-
 
         elif cell.value == '121.51.9':
             wb2 = openpyxl.load_workbook(caminho + cell.value.replace('.', '') + '.xlsx', data_only=True)
@@ -235,8 +249,8 @@ for row in ws.iter_rows(min_row=6, min_col=6, max_col=6, max_row=727):
             wb2 = openpyxl.load_workbook(caminho + cell.value.replace('.', '') + '.xlsx', data_only=True)
             sheets = wb2.sheetnames
             ws2 = wb2[sheets[0]]
-            ws.cell(row=cell.row, column=pos_coluna).value = 0
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = 0
+            ws.cell(row=cell.row, column=pos_coluna).value = (ws2['D5'].value or 0) * -1
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = (ws2['C8'].value or 0) * -1
             ws.cell(row=cell.row + 2, column=pos_coluna).value = (ws2['C7'].value or 0) * -1
 
         elif cell.value == '221.99.3':
@@ -260,19 +274,19 @@ for row in ws.iter_rows(min_row=6, min_col=6, max_col=6, max_row=727):
 
             ws.cell(row=cell.row, column=pos_coluna).value = (ws2['D5'].value or 0) * -1
             ws.cell(row=cell.row + 1, column=pos_coluna).value = 0
+            ws.cell(row=cell.row + 2, column=pos_coluna).value = 0
             # ws.cell(row=cell.row + 2, column=pos_coluna).value = citygate * -1
-            ws.cell(row=cell.row + 2, column=pos_coluna).value = (ws2['I8'].value or 0) * -1
+            ws.cell(row=cell.row + 3, column=pos_coluna).value = (ws2['I8'].value or 0) * -1
             # ws.cell(row=cell.row + 3, column=pos_coluna).value = multa * -1
-            ws.cell(row=cell.row + 3, column=pos_coluna).value = (ws2['I9'].value or 0) * -1
+            ws.cell(row=cell.row + 4, column=pos_coluna).value = (ws2['I9'].value or 0) * -1
             # ws.cell(row=cell.row + 4, column=pos_coluna).value = ambiental * -1
-            ws.cell(row=cell.row + 4, column=pos_coluna).value = (ws2['I10'].value or 0) * -1
             ws.cell(row=cell.row + 5, column=pos_coluna).value = 0
             # ws.cell(row=cell.row + 6, column=pos_coluna).value = pis * -1
-            ws.cell(row=cell.row + 6, column=pos_coluna).value = (ws2['I12'].value or 0) * -1
+            ws.cell(row=cell.row + 6, column=pos_coluna).value = (ws2['I11'].value or 0) * -1
             ws.cell(row=cell.row + 7, column=pos_coluna).value = 0
             # ws.cell(row=cell.row + 8, column=pos_coluna).value = igas * -1
-            ws.cell(row=cell.row + 8, column=pos_coluna).value = (ws2['I13'].value or 0) * -1
-            ws.cell(row=cell.row + 9, column=pos_coluna).value = (ws2['I11'].value or 0) * -1
+            ws.cell(row=cell.row + 8, column=pos_coluna).value = (ws2['I12'].value or 0) * -1
+            ws.cell(row=cell.row + 9, column=pos_coluna).value = (ws2['I10'].value or 0) * -1
 
         elif cell.value == '211.99.9':
             wb4 = openpyxl.load_workbook(caminho + cell.value.replace('.', '') + '.xlsx', data_only=True)
@@ -285,15 +299,16 @@ for row in ws.iter_rows(min_row=6, min_col=6, max_col=6, max_row=727):
             ws.cell(row=cell.row + 4, column=pos_coluna).value = (ws4['H6'].value or 0) * -1
             ws.cell(row=cell.row + 5, column=pos_coluna).value = (ws4['G14'].value or 0) * -1
             ws.cell(row=cell.row + 6, column=pos_coluna).value = 0
-            ws.cell(row=cell.row + 7, column=pos_coluna).value = 0
+            ws.cell(row=cell.row + 7, column=pos_coluna).value = (ws4['G13'].value or 0) * -1
             ws.cell(row=cell.row + 8, column=pos_coluna).value = 0
 
-        elif cell.value == '213.03.1':
+        elif cell.value == '223.03.1':
             wb2 = openpyxl.load_workbook(caminho + cell.value.replace('.', '') + '.xlsx', data_only=True)
             sheets = wb2.sheetnames
             ws2 = wb2[sheets[0]]
-            ws.cell(row=cell.row, column=pos_coluna).value = ws2['C7'].value * -1
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = ws2['C8'].value * -1
+            ws.cell(row=cell.row, column=pos_coluna).value = ws2['D5'].value * -1
+            ws.cell(row=cell.row+1, column=pos_coluna).value = ws2['C7'].value * -1
+            ws.cell(row=cell.row+2, column=pos_coluna).value = ws2['C8'].value * -1
 
         if cell.value == '615.03.1.1.01.01':
             ws.cell(row=cell.row, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value - ppr
@@ -331,54 +346,54 @@ for row in ws.iter_rows(min_row=6, min_col=6, max_col=6, max_row=727):
             ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_pisco[4] * -1
         if cell.value == '615.03.2.1.31':
             ws.cell(row=cell.row, column=pos_coluna).value = \
-                ws.cell(row=cell.row, column=pos_coluna).value - dicionario['6152131002']
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = dicionario['6152131002']
+                ws.cell(row=cell.row, column=pos_coluna).value - contas_custo['6152131002']
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = contas_custo['6152131002']
             ws.cell(row=cell.row + 2, column=pos_coluna).value = 0
         if cell.value == '615.03.3.1.31':
             ws.cell(row=cell.row, column=pos_coluna).value = \
-                ws.cell(row=cell.row, column=pos_coluna).value - dicionario['6153131002']
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = dicionario['6153131002']
+                ws.cell(row=cell.row, column=pos_coluna).value - contas_custo['6153131002']
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = contas_custo['6153131002']
             ws.cell(row=cell.row + 2, column=pos_coluna).value = 0
         if cell.value == '615.03.4.1.31':
             ws.cell(row=cell.row, column=pos_coluna).value = \
-                ws.cell(row=cell.row, column=pos_coluna).value - dicionario['6154131002']
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = dicionario['6154131002']
+                ws.cell(row=cell.row, column=pos_coluna).value - contas_custo['6154131002']
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = contas_custo['6154131002']
             ws.cell(row=cell.row + 2, column=pos_coluna).value = 0
         if cell.value == '615.03.6.1.31':
             ws.cell(row=cell.row, column=pos_coluna).value = \
-                ws.cell(row=cell.row, column=pos_coluna).value - dicionario['6156131002']
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = dicionario['6156131002']
+                ws.cell(row=cell.row, column=pos_coluna).value - contas_custo['6156131002']
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = contas_custo['6156131002']
             ws.cell(row=cell.row + 2, column=pos_coluna).value = 0
         if cell.value == '112.31.2':
             ws.cell(row=cell.row, column=pos_coluna).value = 0
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_50[0]
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_plano50[0]
         if cell.value == '612.03.2.1.02':
             ws.cell(row=cell.row, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value - \
-                                                             lista_50[1]
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_50[1]
+                                                             lista_plano50[1]
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_plano50[1]
         if cell.value == '612.03.3.1.02':
             ws.cell(row=cell.row, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value - \
-                                                             lista_50[2]
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_50[2]
+                                                             lista_plano50[2]
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_plano50[2]
         if cell.value == '616.00.0.1.91.04':
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_50[3]
-            ws.cell(row=cell.row + 2, column=pos_coluna).value = lista_50[4]
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_plano50[3]
+            ws.cell(row=cell.row + 2, column=pos_coluna).value = lista_plano50[4]
         if cell.value == '616.00.0.3.51.01':
             ws.cell(row=cell.row, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value - \
-                                                             lista_50[5]
-            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_50[5]
+                                                             lista_plano50[5]
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = lista_plano50[5]
         if cell.value == '631.00.0.5':
             ws.cell(row=cell.row, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value - \
-                                                            indebito - outras_rec - lista_50[6]
+                                                             indebito - outras_rec - lista_plano50[6]
             ws.cell(row=cell.row + 1, column=pos_coluna).value = outras_rec
-            ws.cell(row=cell.row + 2, column=pos_coluna).value = lista_50[6]
+            ws.cell(row=cell.row + 2, column=pos_coluna).value = lista_plano50[6]
             ws.cell(row=cell.row + 3, column=pos_coluna).value = indebito
         if cell.value == '635.00.0.6':
             ws.cell(row=cell.row, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value
             ws.cell(row=cell.row + 1, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value - \
-                                                                 outras_desp - (lista_50[7] * -1)
+                                                                 outras_desp - (lista_plano50[7])
             ws.cell(row=cell.row + 2, column=pos_coluna).value = outras_desp
-            ws.cell(row=cell.row + 3, column=pos_coluna).value = lista_50[7] * -1
+            ws.cell(row=cell.row + 3, column=pos_coluna).value = lista_plano50[7]
         if cell.value == '616.00.0.3.42.01':
             ws.cell(row=cell.row + 1, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value
         if cell.value == '631.00.0.2':
@@ -386,11 +401,22 @@ for row in ws.iter_rows(min_row=6, min_col=6, max_col=6, max_row=727):
         if cell.value == '635.00.0.2':
             ws.cell(row=cell.row + 1, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value
         if cell.value == '651.00.0.1.01':
-            ws.cell(row=cell.row + 2, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = vag2
+            ws.cell(row=cell.row + 2, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value - vag2
         if cell.value == '652.00.0.1.01':
-            ws.cell(row=cell.row + 2, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value
+            ws.cell(row=cell.row + 2, column=pos_coluna).value = lista_plano50[8]
+            ws.cell(row=cell.row + 3, column=pos_coluna).value = vag
         if cell.value == '671.03.0.3':
             ws.cell(row=cell.row + 1, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value
-
+        if cell.value == '675.03.0.3':
+            ws.cell(row=cell.row, column=pos_coluna).value = 0
+            ws.cell(row=cell.row+1, column=pos_coluna).value = prov_ind
+            ws.cell(row=cell.row+2, column=pos_coluna).value = deliberacao
+            ws.cell(row=cell.row+3, column=pos_coluna).value = termok
+        if cell.value == '616.00.0.3.94':
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value
+        if cell.value == '616.00.0.3.94.01':
+            ws.cell(row=cell.row + 1, column=pos_coluna).value = ws.cell(row=cell.row, column=pos_coluna).value
+            ws.cell(row=cell.row, column=pos_coluna).value = 0
 
 wb.save('DF2.xlsx')
